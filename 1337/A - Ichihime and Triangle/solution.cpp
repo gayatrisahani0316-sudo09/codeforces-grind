b42b168e52f155209f12ad48fda999f43814ef35
Gayatri <gayatrisahani0316@gmail.com>
2026-07-06 23:44:53 +0530

    Add Ichihime and Triangle [A] from Codeforces

---
+++ 1337/A - Ichihime and Triangle/solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+void solve(){
+    long long a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << endl;
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
